768bdd3f8fcd921429492d333b8f6e9dcd938115
Choloooo <89234566+Choloooo@users.noreply.github.com>
2024-04-06 10:31:34 +0100

    refactoring and adding of coming soon features

--- fileconverter/urls.py
+++ fileconverter/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import convert_to_pdf
+from fileconverter import views
 
 urlpatterns = [
-    path('', convert_to_pdf, name='convert'),
+    path('', views.word_to_pdf, name='word2pdf'),
+    path('PDF2Word/', views.pdf_to_word, name='PDF2Word'),
+
+
+    path('PPT2PDF/', views.ppt_to_pdf, name='PPT2PDF'),
+    path('MergePDF/', views.merge_pdf, name='MergePDF'),
+    path('SignPDF/', views.sign_pdf, name='SignPDF'),
 
 ]
+
+
+
+
+
